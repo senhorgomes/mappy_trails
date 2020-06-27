94b05f75d80f6f758612d94276f9479b982c0888
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-27 20:36:30 +0000

    Added users.sql

---
+++ db/seeds/03_maps.sql
@@ -0,0 +1,4 @@
+-- Maps table seeds here
+INSERT INTO maps VALUES (id, name, description, category, is_favourite, owner_id) VALUES (1, 'Cozy Cafes', 'Cafes', 'Coziest Cafes in downtown Toronto', DEFAULT, 1);
+INSERT INTO maps VALUES (id, name, description, category, is_favourite, owner_id) VALUES (2, 'Cafes with the tastiest bakeries', 'Cafes', 'Tastiest treats and possibly the best scones in Toronto.', DEFAULT, 4);
+INSERT INTO maps VALUES (id, name, description, category, is_favourite, owner_id) VALUES (2, 'Cafes with the tastiest bakeries', 'Cafes', 'Tastiest treats and possibly the best scones in Toronto.', DEFAULT, 4);
---
+++ db/seeds/04_pointsmaps.sql
@@ -0,0 +1,18 @@
+-- points-maps table seeds here
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (1, 1, 1);
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (2, 2, 1);
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (3, 3, 1);
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (4, 4, 1);
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (5, 5, 1);
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (6, 6, 1);
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (7, 7, 1);
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (8, 8, 1);
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (9, 9, 1);
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (10, 10, 1);
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (2, 1, 1);
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (2, 1, 1);
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (2, 1, 1);
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (2, 1, 1);
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (2, 1, 1);
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (2, 1, 1);
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES (2, 1, 1);
