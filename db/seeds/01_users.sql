d49cee23a128d7b5bdb8d4f25800996bca7c5783
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-27 17:03:50 +0000

    Started working on git seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,42 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- Users table seeds here
+INSERT INTO users (id, name, email) VALUES (1, 'Alice', 'alice@email.com');
+INSERT INTO users (id, name, email) VALUES (2, 'Kira', 'kira@email.com');
+INSERT INTO users (id, name, email) VALUES (3, 'Bob', 'bob@email.com');
+INSERT INTO users (id, name, email) VALUES (4, 'Kendrick', 'kendrick@email.com');
+
+-- Point table seeds here
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (1, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.', 43.6550893, -79.4021496, DEFAULT, 1)
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (2, 'Moonbean Coffee Company', 'Homey spot for espresso drinks made from house-roasted beans, plus snacks & over 30 loose-leaf teas.', 43.6542208, -79.400205, DEFAULT, 1)
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (3, 'Little Pebbles', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (4, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (5, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (6, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (7, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (8, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (9, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (10, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (11, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (12, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (13, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (14, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (15, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (16, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (17, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (18, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (19, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (20, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (21, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (22, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (23, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (24, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (25, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (26, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (27, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (28, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (29, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+INSERT INTO points VALUES (id, name, description, latitude, longitude, is_private, owner_id, map_id) VALUES (30, 'Jimmy\'s Coffee', 'Local gourmet coffee chain serving innovative pastries amid industrial-chic decor & art.')
+
+-- Maps table seeds here
+INSERT INTO maps VALUES (id, name, description, category, is_favourite, owner_id) VALUES ();
+-- points-maps table seeds here
+INSERT INTO points-maps VALUES (id, point_id, map_id) VALUES ();
