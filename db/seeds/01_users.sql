89284857518798b456fa1db08e363e312ee75af5
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-27 21:41:04 +0000

    Adding a new favourtie table, switching ID keys from int to serial

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,5 @@
 -- Users table seeds here
-INSERT INTO users (id, name, email) VALUES (1, 'Alice', 'alice@email.com');
-INSERT INTO users (id, name, email) VALUES (2, 'Kira', 'kira@email.com');
-INSERT INTO users (id, name, email) VALUES (3, 'Robert', 'robert@email.com');
-INSERT INTO users (id, name, email) VALUES (4, 'Kendrick', 'kendrick@email.com');
+INSERT INTO users (id, name, email) VALUES (DEFAULT, 'Alice', 'alice@email.com');
+INSERT INTO users (id, name, email) VALUES (DEFAULT, 'Kira', 'kira@email.com');
+INSERT INTO users (id, name, email) VALUES (DEFAULT, 'Robert', 'robert@email.com');
+INSERT INTO users (id, name, email) VALUES (DEFAULT, 'Kendrick', 'kendrick@email.com');
