bdc1d37ba94d23c13f48e00e736c89febfc2091c
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-28 02:03:17 +0000

    Finished seeds and schemas

--- db/schema/03_maps.sql
+++ db/schema/03_maps.sql
@@ -1,10 +1,8 @@
-DROP TABLE IF EXISTS points CASCADE;
-CREATE TABLE points (
+DROP TABLE IF EXISTS maps CASCADE;
+CREATE TABLE maps (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   description VARCHAR(255) NOT NULL,
-  latitude Decimal(8,6) NOT NULL,
-  longitude Decimal(9,6) NOT NULL,
+  category VARCHAR(255) NOT NULL,
   owner_id INTEGER REFERENCES users(id)
 );
-
