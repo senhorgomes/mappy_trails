f9a7e5e67a3f0242b60716a59be7f04b985a7af9
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-06-30 00:46:45 +0000

    Added favorite button, including JS, CSS, and HTML to make it work.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,14 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   for(user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });;
+  let likeBtn = document.querySelector('.ico');
+  likeBtn.addEventListener('click', function () {
+    likeBtn.classList.toggle('liked');
+  });
 });
