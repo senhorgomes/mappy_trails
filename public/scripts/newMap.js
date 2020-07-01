a464bee816a67c0885b75e9da86e2e110999dd96
Bryan Gomes <bryanpgomes@gmail.com>
2020-07-01 20:59:30 +0000

    Adding functionality of geocoding

--- public/scripts/newMap.js
+++ public/scripts/newMap.js
@@ -1,7 +1,9 @@
 let newMapOutput = undefined;
-
+const geocoder = new google.maps.Geocoder();
 $(() => {
-  $('#new_point').hide()
+  $('#new_point').hide();
+  $('#new_point_address').hide();
+  //On submission, it will hide the submission button and populate a form to add a point address
   $('#new_map').on('submit', (evt) => {
     evt.preventDefault();
     console.log("ready");
@@ -12,15 +14,35 @@ $(() => {
       .then((res) => {
         newMapOutput = res;
         console.log(res);
-        $('#new_point').slideDown('slow');
+        $('#new_point_address').slideDown('slow');
         $('#new_map button').hide();
-        //$('new_point.children').empty()
       });
   });
-  $('#new_point').on('submit', (evt) => {
+  //Submit point address for geocoding
+  $('#new_point_address').on('submit', (evt) => {
     evt.preventDefault();
     console.log("ready");
     $.ajax(`/maps/${newMapOutput.id}/points`, {
+      method: 'POST', data: $('form')
+        .serialize()
+    })
+      .then((res) => {
+        function geocodeAddress(geocoder, resultsMap) {
+          var address = $('#new_point_address').value;
+          geocoder.geocode(
+            {
+              address: address
+            });
+        console.log(res);
+        $('#new_point').slideDown('slow');
+        $('#new_point_address button').hide();
+      });
+  })
+  //Final submission to point
+  $('#new_point').on('submit', (evt) => {
+    evt.preventDefault();
+    console.log("ready");
+    $.ajax(`/maps/${newMapOutput.id}/points/submission`, {
       method: 'POST', data: $('form')
         .serialize()
     })
